c4d1e3b528b95a56ac785293584cc6ef567ee4df
Chris Berry and Mike Gallegos <student@epicodus.com>
2016-01-19 13:35:55 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+// //Business Logic
+//
+// var leapYear = function(year) {
+//
+// 	// return ((year % 4 === 0) && (year % 100 != 0) || (year % 400 === 0));
+//
+// 	if ( year > 0 && ((year % 4 === 0) && (year % 100 !== 0)|| (year % 400 === 0))) {
+// 		return true;
+// 	} else if (year === NaN) {
+// 		return false;
+// 	} else {
+// 		return false;
+// 	}
+
+	// if (year % 100 === 0) {
+	// 	return false;
+	// } else if
+	// 	(year % 4 === 0) {
+	// 	return true;
+	// } else {
+	// return false;
+	// }
+// };
+//
+// // UI Logic
+//
+// $(document).ready(function() {
+//   $("form#leap-year").submit(function(event) {
+//      var year = parseInt($("input#year").val());
+//      var result = leapYear(year);
+//
+//     $(".year").text(year);
+//
+// 		if (isNaN(year) || year < 0 ) {
+// 			alert("OMG INVALID INPUT");
+// 			$(".not").text("not");
+// 		}
+//     else if (!result) {       // same as writing if (result === false)
+//       $(".not").text("not");
+// 			$("#result").show();
+//     } else {
+//       $(".not").text("");
+// 			$("#result").show();
+//     }
+//
+//
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,31 @@
+
+// first argument 'leapYear' is the function we intend to write
+// second argument is the it function. The it function takes a
+// string as its first argument, too. The second argument is
+// the function which contains the assertion.
+
+// describe ('leapYear', function() {
+// 	it("is false for a year that is not divisible by 4, 100, or 400", function() {
+// 		expect(leapYear(1993)).to.equal(false);
+// 	});
+//
+// 	it ("is true for years divisible by 4", function() {
+// 		expect(leapYear(2004)).to.equal(true);
+// 	});
+//
+// 	it ("is false for years divisible by 100", function() {
+// 		expect (leapYear(1900)).to.equal(false);
+// 	});
+//
+// 	it ("is true for years divisible by 400", function() {
+// 		expect (leapYear(2000)).to.equal(true);
+// 	});
+//
+// 	it ("is false for blank input", function() {
+// 		expect (leapYear(NaN)).to.equal(false);
+// 	});
+//
+// 	it ("is false for negavtive integers", function() {
+// 		expect (leapYear(-400)).to.equal(false);
+// 	});
+// });
